4be4d2d30784f25cb4979f2b9bdaeb6066ce51a0
Goldack, Nicolas <Nicolas.Goldack@arag.de>
2023-01-13 13:28:52 +0100

    feat: added default values to varaibles in terraform

--- iac/variables.tf
+++ iac/variables.tf
@@ -11,6 +11,7 @@ variable "fly_api_token" {
 # Fly Database
 variable "fly_database_regions" {
   type = set(string)
+  default = [ "fra" ]
 }
 
 variable "fly_database_name" {
@@ -20,31 +21,37 @@ variable "fly_database_name" {
 
 variable "fly_database_image" {
   type = string
+  default = "mongo:latest"
 }
 
 # Fly Database volume
 variable "fly_database_volume_regions" {
     type = set(string)
+    default = [ "fra" ]
 }
 
 variable "fly_database_volume_name" {
     type = string
+    default = "dicertace_database_volume"
 }
 
 variable "fly_database_volume_size" {
     type = number
-    default = 3
+    default = 1
 }
 
 # Fly Backend
 variable "fly_backend_regions" {
     type = set(string)
+    default = [ "fra" ]
 }
 
 variable "fly_backend_name" {
     type = string
+    default = "dicetrace-backend"
 }
 
 variable "fly_backend_image" {
     type = string
+    default = "traefik/whoami:latest"
 }
