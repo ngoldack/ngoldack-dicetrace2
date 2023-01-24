d1c3be99598542ea12841f36ed2556b27ac384bd
Nicolas Goldack <nicolas-goldack@live.de>
2023-01-24 13:36:07 +0100

    refactor(frontend): refactored

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,7 +1,6 @@
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
-    console.log(await event.locals.getSession())
   return {
     session: await event.locals.getSession()
   };
